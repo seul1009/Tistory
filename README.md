d94a4bf33f2235f34ed4e755b6b6ea2f981eef47
SEULI <tmfdl8414@naver.com>
2025-04-22 20:31:12 +0900

    fix

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Tistor<!-- RECENT POST START -->
+- 2025-04-22 [백준 / 13335번 / 트럭 / python 파이썬](https://seulow-down.tistory.com/349)
+- 2025-04-21 [YOLO / CNN 차이](https://seulow-down.tistory.com/348)
 - 2025-04-16 [[Error] 리액트 네이티브 안드로이드 가상 디바이스 연결 오류  No apps connected. sending &quot;reload&quot; to all react native apps failed. make sure your app is running in the simulator or on a phone connected via usb.](https://seulow-down.tistory.com/347)
 - 2025-04-15 [[React Native] 모든 캐시 삭제 / 의존성 재설치](https://seulow-down.tistory.com/346)
 - 2025-04-14 [백준 / 1654번 / 랜선 자르기 / python 파이썬](https://seulow-down.tistory.com/345)
-- 2025-04-11 [[MongoDB] 명령어 모음](https://seulow-down.tistory.com/344)
-- 2025-04-11 [백준 / 1655번 / 가운데를 말해요 / python 파이썬](https://seulow-down.tistory.com/343)
-y
+)
--- test.py
+++ test.py
@@ -1,33 +1,30 @@
 import feedparser
 from datetime import datetime
+import urllib.request
 
 POST_COUNT = 5
 BLOG_RSS_URL = "https://seulow-down.tistory.com/rss"
-feed = feedparser.parse(BLOG_RSS_URL)
+
+# 캐시 우회를 위해 현재 시간으로 쿼리 스트링 추가
+rss_url_with_timestamp = BLOG_RSS_URL + "?" + datetime.now().strftime("%Y%m%d%H%M%S")
+req = urllib.request.Request(rss_url_with_timestamp, headers={"Cache-Control": "no-cache"})
+feed = feedparser.parse(urllib.request.urlopen(req))
 
 entries = feed['entries']
-entries.sort(key=lambda x: datetime.strptime(x.published, '%a, %d %b %Y %H:%M:%S %z'), 
-             reverse=True)
+entries.sort(key=lambda x: datetime.strptime(x.published, '%a, %d %b %Y %H:%M:%S %z'), reverse=True)
 
 posts = []
 for entry in entries[:POST_COUNT]:
-    # 날짜 형식 변경 (yyyy-mm-dd)
     published_date = datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %z').strftime('%Y-%m-%d')
     posts.append(f"- {published_date} [{entry.title}]({entry.link})")
 
-print(posts)
-
 readme_path = './README.md'
 
-# open README.md
 with open(readme_path, 'r', encoding='utf-8') as readme_md_file:
     original_readme = readme_md_file.read()
 
-# README.md 파일에서 <!-- RECENT POST START -->와
-# <!-- RECENT POST END --> 사이의 부분을 찾아 교체
 start_comment = '<!-- RECENT POST START -->'
 end_comment = '<!-- RECENT POST END -->'
-
 start_index = original_readme.find(start_comment)
 end_index = original_readme.find(end_comment)
 
@@ -39,12 +36,9 @@ updated_readme = (
     original_readme[end_index:]
 )
 
-# README.md 파일에 업데이트된 내용 쓰기 (내용이 변경된 경우에만)
 if updated_readme != original_readme:
     with open(readme_path, 'w', encoding='utf-8') as readme_md_file:
         readme_md_file.write(updated_readme)
-
-    # 파일의 길이를 맞추기 위해 남는 부분을 삭제
     with open(readme_path, 'a', encoding='utf-8') as readme_md_file:
         readme_md_file.truncate()
 else:
